17555041e1caf4075b6eeb11caa4fbd67e34ca69
EuMotta <hyperxmotta@hotmail.com>
2023-05-08 09:40:45 -0300

    Horse position convert to int

--- src/app/sections/Horse.jsx
+++ src/app/sections/Horse.jsx
@@ -30,8 +30,8 @@ export default function Horse() {
   };
 
   const handleHorse1 = () => {
-    const newPosition1 = position1 +  Math.random() * (50 - 10) + 10;
-    const newPosition2 = position2 + Math.random() * (50 - 10) + 10;
+    const newPosition1 = Math.floor(position1 + Math.random() * (40 - 10) + 10);
+    const newPosition2 = Math.floor(position2 + Math.random() * (40 - 10) + 10);
     setPosition1(newPosition1);
     setPosition2(newPosition2);
     setHistory([...history, position1, position2]);
@@ -47,38 +47,49 @@ export default function Horse() {
   return (
     <main>
       <div className="">
-          <div className="p-5">
-            {horses.map(item => (
-              <div className="bg-road">
-                <div
-                  className="relative w-14"
-                  style={{
-                    left: item.position === 'position1' ? position1 : position2,
-                  }}
-                >
-                  <Image
-                    className="scale-x-[-1]"
-                    src={item.image}
-                    width={50}
-                    height={50}
-                  />
-                </div>
+        <div className="p-5">
+          {horses.map(item => (
+            <div className="bg-road">
+              <div
+                className="relative w-14"
+                style={{
+                  left: item.position === 'position1' ? position1 : position2,
+                }}
+              >
+                <Image
+                  className="scale-x-[-1]"
+                  src={item.image}
+                  width={50}
+                  height={50}
+                />
               </div>
-            ))}
+            </div>
+          ))}
+        </div>
+        <div className="p-5 bg-gray-100 w-1/4 rounded-lg shadow-md">
+          <div className="text-lg font-semibold mb-2">{displayText}</div>
+          <div className="border p-2 rounded-lg mb-2">{position1}</div>
+          <div className="border p-2 rounded-lg mb-4">{position2}</div>
+          <div className="flex gap-4">
+            <button
+              className=" bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+              onClick={handleUndo}
+            >
+              Voltar
+            </button>
+            <button
+              className=" bg-green-500 hover:bg-green-700 text-white text-sm font-bold py-2 px-4 rounded"
+              onClick={handleHorse1}
+            >
+              Avançar
+            </button>
+            <button
+              className=" bg-red-500 hover:bg-red-700 text-white text-sm font-bold py-2 px-4 rounded"
+              onClick={resetPosition}
+            >
+              Resetar
+            </button>
           </div>
-        <div>{displayText}</div>
-        <div className="border">{position1}</div>
-        <div className="border">{position2}</div>
-        <div className="flex p-5 gap-5">
-          <button className="button-back" onClick={handleUndo}>
-            Voltar
-          </button>
-          <button className="button-go" onClick={handleHorse1}>
-            Avançar
-          </button>
-          <button className="button-go" onClick={resetPosition}>
-            Resetar
-          </button>
         </div>
       </div>
     </main>
