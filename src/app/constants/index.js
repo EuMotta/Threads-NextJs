4f95797ad23d1d8eb7c3bf66e919901c495439ee
EuMotta <hyperxmotta@hotmail.com>
2023-05-09 10:20:30 -0300

    description + example

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -13,3 +13,29 @@ export const horses = [
     position: 'position2',
   }
 ]
+
+export const explication = {
+
+  textExample: [
+    {
+      value: 20,
+      formula: 'Math.random() * (20 - 10) + 20',
+      description: 'Valor entre 10 e 20'
+    },
+    {
+      value: 30,
+      formula: 'Math.random() * (30 - 10) + 30',
+      description: 'Valor entre 20 e 30'
+    },
+    {
+      value: 40,
+      formula: 'Math.random() * (40 - 10) + 40',
+      description: 'Valor entre 30 e 40'
+    },
+    {
+      value: 50,
+      formula: 'Math.random() * (50 - 10) + 50',
+      description: 'Valor entre 40 e 50'
+    }
+  ]
+}
--- src/app/sections/Horse.jsx
+++ src/app/sections/Horse.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 import '../../styles/Horse.css';
 import horse1 from '../../images/horse1.svg';
-import { horses } from '../constants';
+import { explication, horses } from '../constants';
 import { AiOutlineBorderRight } from 'react-icons/ai';
 import { MdSocialDistance } from 'react-icons/md';
 import { GiStarsStack } from 'react-icons/gi';
@@ -153,87 +153,165 @@ export default function Horse() {
             </div>
           ))}
         </div>
-        <div className="grid grid-cols-4">
-          <div className="p-5 mt-5 bg-gray-100  rounded-lg shadow-md">
-            <div className="flex">
-              <div className=" w-5/6">
-                <div className="text-lg font-semibold mb-2">{displayText}</div>
-                <div className="border p-2 rounded-lg mb-2">{position1}</div>
-                <div className="border p-2 rounded-lg mb-4">{position2}</div>
-              </div>
-              <div className="w-1/6 p-2 flex flex-col justify-center items-center">
-                <button onClick={handleShowBorder}>
-                  <AiOutlineBorderRight className="text-2xl" />
-                </button>
-                <button onClick={handleShowDistance}>
-                  <MdSocialDistance className="text-2xl" />
-                </button>
+        <div className="grid grid-cols-4 gap-5">
+          <div className="col-span-1">
+            <div className="">
+              <div className="p-5 mt-5 bg-gray-100  rounded-lg shadow-md">
+                <div className="flex">
+                  <div className=" w-5/6">
+                    <div className="text-lg font-semibold mb-2">
+                      {displayText}
+                    </div>
+                    <div className="border p-2 rounded-lg mb-2">
+                      {position1}
+                    </div>
+                    <div className="border p-2 rounded-lg mb-4">
+                      {position2}
+                    </div>
+                  </div>
+                  <div className="w-1/6 p-2 flex flex-col justify-center items-center">
+                    <button onClick={handleShowBorder}>
+                      <AiOutlineBorderRight className="text-2xl" />
+                    </button>
+                    <button onClick={handleShowDistance}>
+                      <MdSocialDistance className="text-2xl" />
+                    </button>
+                  </div>
+                </div>
+                <div className="flex gap-4 flex-wrap">
+                  <button
+                    className=" bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                    onClick={handleUndo}
+                  >
+                    Voltar
+                  </button>
+                  <button
+                    className=" bg-green-500 hover:bg-green-700 text-white text-sm font-bold py-2 px-4 rounded"
+                    onClick={handleHorse1}
+                  >
+                    Avançar
+                  </button>
+                  <button
+                    className=" bg-red-500 hover:bg-red-700 text-white text-sm font-bold py-2 px-4 rounded"
+                    onClick={resetPosition}
+                  >
+                    Resetar
+                  </button>
+                  <button
+                    className=" bg-violet-500 hover:bg-violet-700 text-white text-sm font-bold py-2 px-4 rounded"
+                    onClick={startRace}
+                  >
+                    auto
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="flex gap-4 flex-wrap">
-              <button
-                className=" bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
-                onClick={handleUndo}
-              >
-                Voltar
-              </button>
-              <button
-                className=" bg-green-500 hover:bg-green-700 text-white text-sm font-bold py-2 px-4 rounded"
-                onClick={handleHorse1}
-              >
-                Avançar
-              </button>
-              <button
-                className=" bg-red-500 hover:bg-red-700 text-white text-sm font-bold py-2 px-4 rounded"
-                onClick={resetPosition}
-              >
-                Resetar
-              </button>
-              <button
-                className=" bg-violet-500 hover:bg-violet-700 text-white text-sm font-bold py-2 px-4 rounded"
-                onClick={startRace}
-              >
-                auto
-              </button>
+            <div className="p-5 mt-5 bg-gray-100 flex  rounded-lg shadow-md">
+              <div className="flex gap-5 flex-col">
+                <div className="flex">
+                  <input
+                    type="number"
+                    value={speed}
+                    min={13}
+                    max={600}
+                    onChange={e => {
+                      const value = e.target.value;
+                      if (value >= 13 && value <= 600) {
+                        setSpeed(value);
+                      }
+                    }}
+                    className="shadow-sm shadow-slate-500 rounded-md p-2 w-1/4"
+                  />
+                  <div className="text-lg font-bold flex  justify-center items-center ml-2">
+                    Velocidade
+                  </div>
+                </div>
+                <div className="flex">
+                  <input
+                    type="number"
+                    value={speedInterval}
+                    onChange={e => setSpeedInterval(e.target.value)}
+                    className="shadow-sm shadow-slate-500 rounded-md p-2 w-1/4"
+                  />
+                  <div className="text-lg font-bold flex justify-center items-center ml-2">
+                    Intervalo
+                  </div>
+                </div>
+              </div>
+              <div className="p-2  flex flex-col justify-center items-center">
+                <button
+                  className="hover:scale-110 transition-all text-2xl"
+                  onClick={() => {
+                    setSpeed(30);
+                    setSpeedInterval(400);
+                  }}
+                >
+                  <GrPowerReset />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="p-5 mt-5 bg-gray-100 flex w-1/4 rounded-lg shadow-md">
-          <div className="flex gap-5 flex-col">
-            <div className="flex">
-              <input
-                type="number"
-                value={speed}
-                min={13}
-                max={600}
-                onChange={e => {
-                  const value = e.target.value;
-                  if (value >= 13 && value <= 600) {
-                    setSpeed(value);
-                  }
-                }}
-                className="shadow-sm shadow-slate-500 rounded-md p-2 w-1/4"
-              />
-              <div className="text-lg font-bold flex  justify-center items-center ml-2">
-                Velocidade
+          <div className="col-span-3">
+            <div className="p-5 mt-5 bg-gray-100 rounded-lg shadow-md">
+              <h1 className="text-2xl font-bold mb-4">
+                Threads em uma corrida de cavalos
+              </h1>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">
+                  O que são threads?
+                </h2>
+                <p className="mb-4">
+                  Threads são uma maneira de dividir um programa em duas ou mais
+                  tarefas simultâneas que podem ser executadas
+                  independentemente. Isso significa que cada thread pode
+                  executar uma parte diferente do código ao mesmo tempo que
+                  outras threads. Isso pode ser útil quando você tem várias
+                  tarefas que precisam ser executadas ao mesmo tempo, mas não
+                  dependem uma da outra para serem concluídas.
+                </p>
+              </div>
+              <div>
+                <h2 className="text-xl font-semibold mb-2">
+                  Como usar threads em uma corrida de cavalos?
+                </h2>
+                <p className="mb-4">
+                  Em uma simulação de corrida de cavalos com dois cavalos, você
+                  pode usar threads para controlar o movimento de cada cavalo de
+                  forma independente. Cada thread pode ser responsável por
+                  atualizar a posição do cavalo na pista e verificar se ele
+                  cruzou a linha de chegada.
+                  <br /> Os cavalos precisam de uma margem de variação em sua
+                  velocidade, se a velocidade máxima for 50, varia entre 40 a
+                  50.
+                </p>
+              </div>
+              <div className="p-5 mt-5 bg-gray-100 rounded-lg shadow-md shadow-slate-500">
+                <h1 className="text-2xl font-bold mb-4">
+                  Exemplos de variação do cálculo
+                </h1>
+                <table className="table-auto w-full mb-4 text-center">
+                  <thead>
+                    <tr className="text-center">
+                      <th className="px-4 py-2 ">Speed</th>
+                      <th className="px-4 py-2 ">Calculo</th>
+                      <th className="px-4 py-2 ">Resultado</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {explication.textExample.map(item => (
+                      <tr>
+                      <td>{item.value}</td>
+                      <td>
+                        {item.formula}
+                      </td>
+                      <td >{item.description}</td>
+                    </tr>
+                    ))}
+                    
+                  </tbody>
+                </table>
               </div>
             </div>
-            <div className="flex">
-              <input
-                type="number"
-                value={speedInterval}
-                onChange={e => setSpeedInterval(e.target.value)}
-                className="shadow-sm shadow-slate-500 rounded-md p-2 w-1/4"
-              />
-              <div className="text-lg font-bold flex justify-center items-center ml-2">
-                Intervalo
-              </div>
-            </div>
-          </div>
-          <div className="p-2  flex flex-col justify-center items-center">
-            <button className="hover:scale-110 transition-all text-2xl" onClick={() => { setSpeed(30); setSpeedInterval(400); }}>
-              <GrPowerReset />
-            </button>
           </div>
         </div>
       </div>
